6f63bbed28d869e1db665489ce3da9b3d621d430
michael robinson <michael.robinson@mikemrobinsondev.com>
2022-07-03 10:19:53 -0400

    Adding files...

---
+++ Migrations/20220702203006_003.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.Migrations
+{
+    public partial class _003 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "UserAcceptedTerms",
+                table: "AspNetUsers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserAcceptedTerms",
+                table: "AspNetUsers");
+        }
+    }
+}
